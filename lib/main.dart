fa3103e1088bd33727a2395535bff59803b61b17
I438161 <p.vanhooren@student.fontys.nl>
2022-12-16 16:22:10 +0100

    Fix fix fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:bletest/comms/hive/adaptors/hiveEntryRepository.dart';
-<<<<<<< HEAD
 import 'package:bletest/comms/hive/adaptors/hiveProfileRepository.dart';
 import 'package:bletest/comms/hive/adaptors/settingsRepository.dart';
 import 'package:bletest/comms/hive/hiveConfig.dart';
 import 'package:bletest/notifications/notification_manager.dart';
-=======
 import 'package:bletest/comms/hive/adaptors/hiveMomentRepository.dart';
-import 'package:bletest/comms/hive/hiveConfig.dart';
 import 'package:bletest/moment/moment_manager.dart';
->>>>>>> f3-moments
 import 'package:bletest/pages/navigation_page.dart';
 import 'package:bletest/pages/theme_config.dart';
 import 'package:bletest/profile/profile_manager.dart';
