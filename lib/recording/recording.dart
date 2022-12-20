7e66e483c1faa2810b07a73712b36b0a77df59ec
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-12-20 14:00:37 +0100

    fixed reference time

--- lib/recording/recording.dart
+++ lib/recording/recording.dart
@@ -11,8 +11,8 @@ class Recording {
     int i = -1;
     for (int i2 = 0; i2 <= bArr.length; i2++) {
       if (i2 == bArr.length ||
-          (bArr[i2]).toInt() == 252 ||
-          (bArr[i2]).toInt() == 253) {
+          (bArr[i2] & 255).toInt() == 252 ||
+          (bArr[i2] & 255).toInt() == 253) {
         if (i != -1) {
           if (i2 - i > 8) {
             Session? decode = Session.decode(bArr.getRange(i, i2).toList());
